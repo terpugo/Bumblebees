c0f288e7026be13eeb4276792567855d18760ff7
Janae <rbrownek@terpmail.umd.edu>
2025-11-04 00:00:03 -0500

    Auto backup: 2025-11-04 00:00:03 running from crontab

---
+++ slack_error.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <channel> <message>"
+  exit 1
+fi
+
+channel=$1
+message=$2
+
+# Sanitize message to remove quotes
+message=$(echo "$message" | tr -d '"' | tr -d "'")
+
+# GITIGNORE THIS FILE!!!!! IMPORTANT!!!
+url=$(cat /home/aces/Bumblebees/url_error.env)
+
+# Formats the message properly
+get_data_json() {
+  cat <<EOF
+{ "channel": "$channel", "icon_emoji": ":bot:", "text": "$message" }
+EOF
+}
+
+# Sends the message to Slack webhook
+/usr/bin/curl -X POST -H 'Content-type: application/json; charset=utf-8' --data "$(get_data_json)" $url
